060470ef1f9c9b0be4af6720f018da95462360c7
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2025-12-22 21:30:03 -0500

    correccion de errores
    se corrigio un error relacionado con las referencias que impedia construir correctamente la Api ya que se producia una interferencia, tambien se movio la carpeta PersonsApi dentro de backend

---
+++ Fullstack/Backend/PersonsApi/Program.cs
@@ -0,0 +1,39 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using PersonsApi.Repositories;
+using PersonsApi.Validators;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Servicios
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Repositorio en memoria
+builder.Services.AddSingleton<IPersonRepository, InMemoryPersonRepository>();
+
+// FluentValidation
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<PersonCreateValidator>();
+
+// CORS
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
